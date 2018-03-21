007ce6dbed36da00b6c0b45c5b6bc0c0b57699a9
CJY <CJY@CJYui-MacBook-Air.local>
2018-03-21 23:17:00 +0900

    high performance api

---
+++ p131_highqual_cnn.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("./mnist/data/",one_hot=True)
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Data variabel
+X = tf.placeholder(tf.float32 , [None, 28, 28, 1])
+Y = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32)
+
+#1st CNN Layer
+#Convolution layer
+# W1 = tf.Variable(tf.random_normal([3,3,1,32],stddev=0.01))
+# L1 = tf.nn.conv2d(X, W1, strides=[1,1,1,1],padding="SAME")
+# L1 = tf.nn.relu(L1)
+# #Pooling laye
+# L1 = tf.nn.max_pool(L1, ksize = [1,2,2,1], strides=[1,2,2,1], padding="SAME")
+L1 = tf.layers.conv2d(X,32,[3,3])
+L1 = tf.layers.max_pooling2d(L1, [2,2], [2,2])
+
+#2nd CNN Layer
+#Convolution layer
+# W2 = tf.Variable(tf.random_normal([3,3,32,64],stddev=0.01))
+# L2 = tf.nn.conv2d(L1,W2, strides=[1,1,1,1],padding="SAME")
+# L2 = tf.nn.relu(L2)
+# #Pooling Layer
+# L2 = tf.nn.max_pool(L2, ksize=[1,2,2,1], strides = [1,2,2,1], padding="SAME")
+L2 = tf.layers.conv2d(L1,64,[3,3])
+L2 = tf.layers.max_pooling2d(L2, [2,2], [2,2])
+
+#Final CNN Layer
+#Convolution Layer
+# W3 = tf.Variable(tf.random_normal([7*7*64, 256],stddev=0.01))
+# L3 = tf.reshape(L2, [-1, 7*7*64]) #L2 pooling data -> 1-D data
+# L3 = tf.matmul(L3, W3)
+# L3 = tf.nn.relu(L3)
+L3 = tf.contrib.layers.flatten(L2)
+L3 = tf.layers.dense(L3, 256, activation=tf.nn.relu)
+ 
+#Output Layer
+# W4 = tf.Variable(tf.random_normal([256,10],stddev=0.01))
+# model = tf.matmul(L3, W4)
+model = tf.layers.dense(L3, 10, activation=None)
+
+#Cost function & optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=model, labels=Y))
+optimizer= tf.train.AdamOptimizer(0.001).minimize(cost)
+#alternative optimizer = tf.train.RMSPropOptimizer(0.001,0.9).minimize(cost)
+# batch_xs.reshape(-1, 28, 28, 1)
+# mnist.test.images.reshape(-1,28,28,1)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+batch_size = 100
+total_batch = int(mnist.train.num_examples / batch_size)
+for epoch in range(1):
+    total_cost = 0
+    
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        batch_xs = batch_xs.reshape(-1,28,28,1)
+
+        _, cost_val = sess.run([optimizer, cost], feed_dict={X:batch_xs, Y:batch_ys, keep_prob:0.7})
+        total_cost += cost_val
+    print('Epoch:','%04d' % (epoch +1), 'Avg cost:','{:.3f}'.format(total_cost / total_batch))
+print("최적화 완료")
+
+#Result
+is_correct = tf.equal(tf.argmax(model,1),tf.argmax(Y,1))
+accuracy = tf.reduce_mean(tf.cast(is_correct,tf.float32))
+print("정확도:",sess.run(accuracy, feed_dict={X:mnist.test.images.reshape(-1,28,28,1), Y:mnist.test.labels, keep_prob:1}))
+
+#Plotting of result
+labels = sess.run(model,feed_dict={X:mnist.test.images.reshape(-1,28,28,1), Y:mnist.test.labels, keep_prob:1})
+fig = plt.figure()
+
+for i in range(10):
+    subplot = fig.add_subplot(2,5,i+1)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.set_title('%d' % np.argmax(labels[i]))
+    subplot.imshow(mnist.test.images[i].reshape((28,28)),cmap=plt.cm.gray_r)
+plt.show()
