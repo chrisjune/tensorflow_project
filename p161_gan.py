61d1b2f1f07c42e0f3b556d609de6ab0044bdb44
CJY <CJY@CJYui-MacBook-Air.local>
2018-03-28 23:22:26 +0900

    Add Gan RandomNumber generating

--- p161_gan.py
+++ p161_gan.py
@@ -1,91 +1,85 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("./mnist/data",one_hot=True)
-total=eopch =100
+mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
+total_epoch = 100
 batch_size = 100
-leanring_rate =0.0002
+leanring_rate = 0.0002
 n_hidden = 256
-n_input =28*28
-n_noise =128
-
-#Original image data
+n_input = 28*28
+n_noise = 128
 X = tf.placeholder(tf.float32, [None, n_input])
-#Fake image data with noise
-Z = tf.placeholder(tf.float32,[None, n_noise])
+Z = tf.placeholder(tf.float32, [None, n_noise])
 
-#Variables for generator
+#Generate fake image
 G_W1 = tf.Variable(tf.random_normal([n_noise, n_hidden], stddev=0.01))
 G_b1 = tf.Variable(tf.zeros([n_hidden]))
-G_W2 = tf.Variable(tf.random_normal([n_hidden,n_input],stddev=0.01))
-G_b2 = tf.Varibale(tf.zeros([n_input]))
+G_W2 = tf.Variable(tf.random_normal([n_hidden, n_input], stddev=0.01))
+G_b2 = tf.Variable(tf.zeros([n_input]))
 
-#Variables for Discriminator
-D_W1 = tf.Variable(tf.random_normal([n_input,n_hidden],stddev=0.01))
+#Discriminate image
+D_W1 = tf.Variable(tf.random_normal([n_input, n_hidden],stddev=0.01))
 D_b1 = tf.Variable(tf.zeros([n_hidden]))
-D_W2 = tf.Variable(tf.random_normal([n_hidden,1],stddev=0.01))
+D_W2 = tf.Variable(tf.random_normal([n_hidden, 1],stddev=0.01))
 D_b2 = tf.Variable(tf.zeros([1]))
 
 def generator(noise_z):
-    hidden = tf.nn.relu(tf.add(tf.matmul(noise_z, G_W1), G_b1))
-    output = tf.nn.sigmoid(tf.add(tf.matmul(hidden,G_W2), G_b2))
+    hidden = tf.nn.relu(tf.matmul(noise_z, G_W1) + G_b1)
+    output =  tf.nn.sigmoid(tf.matmul(hidden, G_W2) + G_b2)
     return output
 
 def discriminator(inputs):
-    hidden = tf.nn.relu(tf.add(tf.matmul(inputs,D_W1),D_b1))
-    ouput = tf.nn.sigmoid(tf.add(tf.matmul(hidden,D_W2),D_b2))
+    hidden = tf.nn.relu(tf.matmul(inputs, D_W1)+D_b1)
+    output = tf.nn.sigmoid(tf.matmul(hidden,D_W2)+D_b2)
     return output
 
 def get_noise(batch_size, n_noise):
-    return np.random_normal(size=(batch_size,n_noise))
+    return np.random.normal(size=(batch_size, n_noise))
 
-#Z(noise)-> G(fake image)
+#Value output
 G = generator(Z)
-#G(fake) -> Discriminate
-D_gene = discriminator(G)
-#X(real) -> Discriminate
 D_real = discriminator(X)
+D_gene = discriminator(G)
 
-#Calculate Loss value
-loss_D = tf.reduce_mean(tf.log(D_real) + tf.log(1 - D_gene))
+#Loss value
+loss_D = tf.reduce_mean(tf.log(D_real)+tf.log(1-D_gene))
 loss_G = tf.reduce_mean(tf.log(D_gene))
 
-#Variable list
-D_var_list = [ D_W1, D_b1, D_W2, D_b2 ]
-G_var_list = [ G_W1, G_b1, G_W2, G_b2 ]
+D_var_list = [D_W1, D_b1, D_W2, D_b2]
+G_var_list = [G_W1, G_b1, G_W2, G_b2]
 
-train_D = tf.train.AdamOptimizer(learning_rate).minimize(-loss_D,var_list=D_var_list)
-train_G = tf.train.AdamOptimizer(learning_rate).minimize(-loss_G,var_list=G_var_list)
+#Maximize loss_D, loss_G
+train_D = tf.train.AdamOptimizer(leanring_rate).minimize(-loss_D,var_list = D_var_list)
+train_G = tf.train.AdamOptimizer(leanring_rate).minimize(-loss_G,var_list = G_var_list)
 
 sess = tf.Session()
-sessrun(tf.global_variables_initializer())
-total_batch = int (mnist.train.num_examples / batch_size )
-loss_val_D, loss_val_G = 0,0
+sess.run(tf.global_variables_initializer())
+
+total_batch = int(mnist.train.num_examples / batch_size)
+loss_val_D,loss_val_G = 0,0
 
 for epoch in range(total_epoch):
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        #sess.run(print(1, batch_xs, batch_ys))
         noise = get_noise(batch_size, n_noise)
 
-        -, loss_val_D = sess.run([train_D, loss_D],feed_dict={X:batch_xs, Z:noise})
-        _, loss_val_G = sess.run([train_G, loss_G],feed_dict={Z: noise})
-    
-    print('Epoch:', '%04d' % epoch,
-        'D_loss: {:.4}'.format(loss_val_D),
-        ,'G_loss: {:.4}'.format(loss_val_G))
-    if epoch == 0 or (epoch+1)%10 == 0 :
+        _,loss_val_D = sess.run([train_D, loss_D], feed_dict={X:batch_xs, Z:noise})
+        _,loss_val_G = sess.run([train_G, loss_G], feed_dict={Z:noise})
+    print('Epoch:','%04d' % epoch, 'D_loss: {:4}'.format(loss_val_D),
+                             'G_loss: {:4}'.format(loss_val_G))
+
+    if epoch == 0 or (epoch + 1)%10 ==0:
         sample_size = 10
-        noise = get_noise(sample_size,n_noise)
-        samples = sess.run(G, feed_dict = {Z:noise}) 
-    
-    fig, ax = plt.subplots(1, sample_size, figsize = (sample_size, 1))
-    for i in range(sample_size):
-        ax[i].sets_axis_off()
-        ax[i].imshow(np.reshape(samples[i], (28,28)))
-    
-    plt.save('samples/{}.png'.format(str(epoch).zfill(3)),
-        bbox_inches ='tight')
-    plt.clost(fig)
-print("최적화완료")
+        noise = get_noise(sample_size, n_noise)
+        samples = sess.run(G,feed_dict={Z:noise})
+        fig, ax = plt.subplots(1, sample_size, figsize=(sample_size,1))
+        for i in range(sample_size):
+            ax[i].set_axis_off()
+            ax[i].imshow(np.reshape(samples[i],(28,28)))
+        plt.savefig('samples/{}.png'.format(str(epoch).zfill(3)),
+                    bbox_inches='tight')
+        plt.close(fig)
+print('최적화완료!')
